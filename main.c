224539fcc574d4773ddcf8d1418b99f2c6d3d920
ssh <sebas.herreraj23@gmail.com>
2021-11-21 22:49:37 -0600

    agregamos chat
    agregamos chat a medias

--- main.c
+++ main.c
@@ -305,7 +305,7 @@ void Search(){
   equipo *ptr;
   ptr=Root;
   char w[10];
-  printf("\nIngrese equipo: ");
+  printf("\nIngrese equipo/colaborador: ");
   scanf("%s",w);
   while(ptr!=NULL && flag==0){
     if(check(w,ptr->nombre)>0)
@@ -411,13 +411,105 @@ void equipos() {
       case 4:
       eliminar();
       break;
-      case 5:exit(0);
+      case 5:menuPrincipal();
     }
   }
   
 }
 
 //----------------------------- TERMINA REGISTRO EQUIPOS ----------------------------
+
+//----------------------------- DOMICILIOS DE COLABORADORES -------------------------
+// A structure to represent an adjacency list node
+struct AdjListNode
+{
+    int dest;
+    struct AdjListNode* next;
+};
+  
+// A structure to represent an adjacency list
+struct AdjList
+{
+    struct AdjListNode *head; 
+};
+  
+// A structure to represent a graph. A graph
+// is an array of adjacency lists.
+// Size of array will be V (number of vertices 
+// in graph)
+struct Graph
+{
+    int V;
+    struct AdjList* array;
+};
+  
+// A utility function to create a new adjacency list node
+struct AdjListNode* newAdjListNode(int dest)
+{
+    struct AdjListNode* newNode =
+     (struct AdjListNode*) malloc(sizeof(struct AdjListNode));
+    newNode->dest = dest;
+    newNode->next = NULL;
+    return newNode;
+}
+  
+// A utility function that creates a graph of V vertices
+struct Graph* createGraph(int V)
+{
+    struct Graph* graph = 
+        (struct Graph*) malloc(sizeof(struct Graph));
+    graph->V = V;
+  
+    // Create an array of adjacency lists.  Size of 
+    // array will be V
+    graph->array = 
+      (struct AdjList*) malloc(V * sizeof(struct AdjList));
+  
+    // Initialize each adjacency list as empty by 
+    // making head as NULL
+    int i;
+    for (i = 0; i < V; ++i)
+        graph->array[i].head = NULL;
+  
+    return graph;
+}
+  
+// Adds an edge to an undirected graph
+void addEdge(struct Graph* graph, int src, int dest)
+{
+    // Add an edge from src to dest.  A new node is 
+    // added to the adjacency list of src.  The node
+    // is added at the beginning
+    struct AdjListNode* newNode = newAdjListNode(dest);
+    newNode->next = graph->array[src].head;
+    graph->array[src].head = newNode;
+  
+    // Since graph is undirected, add an edge from
+    // dest to src also
+    newNode = newAdjListNode(src);
+    newNode->next = graph->array[dest].head;
+    graph->array[dest].head = newNode;
+}
+  
+// A utility function to print the adjacency list 
+// representation of graph
+void printGraph(struct Graph* graph)
+{
+    int v;
+    for (v = 0; v < graph->V; ++v)
+    {
+        struct AdjListNode* pCrawl = graph->array[v].head;
+        printf("\n Adjacency list of vertex %d\n head ", v);
+        while (pCrawl)
+        {
+            printf("-> %d", pCrawl->dest);
+            pCrawl = pCrawl->next;
+        }
+        printf("\n");
+    }
+}
+
+//----------------------------- TERMINA DOMICILIOS DE COLABORADORES ---------------------------
 
 //--------------------------------------- BITACORA TRABAJO --------------------------------------
 struct bitacora
@@ -480,6 +572,42 @@ void aniadirBitacora(BITACORA bitacora) // agrega info de colaborador  a nodo
 
 //--------------------------------------- TERMINA BITACORA TRABAJO ------------------------------
 
+//--------------------------------------- CHAT ---------------------------------------------
+void chat(){
+  int flag=0;
+  equipo *ptr;
+  ptr=Root;
+  char w[10];
+  char y[10];
+  printf("\nIngrese nombre del colaborador emisor: ");
+  scanf("%s",y);
+  printf("\nIngrese nombre del colaborador receptor: ");
+  scanf("%s%*c",w);
+  while(ptr!=NULL && flag==0){
+    if(check(w,ptr->nombre)>0)
+       ptr=ptr->right;
+    else if(check(w,ptr->nombre)<0)
+          ptr=ptr->left;
+    else if(check(w,ptr->nombre)==0){
+       flag=1;
+       printf("Mensaje: ");
+       fgets(ptr->descripcion, 20, stdin);
+       FILE * fpuntero = fopen("equipos.txt", "a+");
+       fwrite(&ptr->nombre, sizeof(BITACORA), 1, fpuntero);
+       fwrite(&ptr->descripcion, sizeof(BITACORA), 1, fpuntero);
+       fclose(fpuntero);
+    }
+
+    }
+    if(flag==0)
+      printf("\nNo se puede establecer conexion con el colaborador\n");
+      //Devolver recorrido
+}
+
+
+
+//--------------------------------------- TERMINA CHAT --------------------------------------
+
 //menu principal
 void menuPrincipal()       
 {
@@ -533,6 +661,7 @@ void menuPrincipal()
             aniadirBitacora(ptr);
             break;
         case 6:
+            chat();
             break;
         case 7:
             break;
